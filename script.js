624b6c428c202e147b2079c7474f0504bada4207
sefaca <sefaca24@gmail.com>
2023-03-22 18:26:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+        // const toggle = document.getElementById('toggleDark');
+        // const body = document.querySelector('body');
+
+        // toggle.addEventListener('click', function() {
+        //     this.classList.toggle('fa-solid fa-toggle-off');
+        // })
+
+var toggle = document.getElementById('container');
+var body = document.querySelector('body');
+
+toggle.onclick = function() {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+
+}
+
+function languageChange(lang) {
+    if (lang == "es") {
+        location.href = "./index.html";
+    } else if (lang == "en") {
+        location.href = "./index-en.html";
+    }
+}
+    
